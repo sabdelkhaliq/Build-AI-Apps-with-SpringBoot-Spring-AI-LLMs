18474670303f665f63587e609d71c897bdd56c7d
Sherif Abdelkhalik <sherifcmp@gmail.com>
2026-06-14 16:29:54 +0200

    chore(lect-17): Core Prompting Techniques with Spring AI

---
+++ SupportTicket/src/test/java/com/udemy/SupportTicket/SupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.SupportTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
